c768ba0995858b4fe3532da689e2d3c8245eeaa5
Steve Banh <steve.banh02@gmail.com>
2025-09-08 01:05:56 -0400

    deleted some files

--- Problem1/HeartRates/Driver.cs
+++ Problem1/HeartRates/Driver.cs
@@ -8,24 +8,24 @@ namespace Problem1
 {
     internal class Driver
     {
-        //static void Main(string[] args)
-        //{
-        //    //Console.WriteLine("Please enter your first name: ");
-        //    //string fName = Console.ReadLine();
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your first name: ");
+            string fName = Console.ReadLine();
 
-        //    //Console.WriteLine("Please enter your last name: ");
-        //    //string lName = Console.ReadLine();
+            Console.WriteLine("Please enter your last name: ");
+            string lName = Console.ReadLine();
 
-        //    //Console.WriteLine("Please enter your birth year name: ");
-        //    //int bYear = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter your birth year name: ");
+            int bYear = int.Parse(Console.ReadLine());
 
-        //    //Console.WriteLine("Please enter the current year name: ");
-        //    //int cYear = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter the current year name: ");
+            int cYear = int.Parse(Console.ReadLine());
 
-        //    //HeartRates myHeartRates = new HeartRates(fName, lName, bYear, cYear);
+            HeartRates myHeartRates = new HeartRates(fName, lName, bYear, cYear);
 
-        //    //myHeartRates.DisplayPatientRecord();
+            myHeartRates.DisplayPatientRecord();
 
-        //}
+        }
     }
 }
